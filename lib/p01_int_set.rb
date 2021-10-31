e926d1b62a91f420a93307727eeabc2f356bb91d
V Prentice <vcoprentice@gmail.com>
2021-10-31 16:49:39 -0700

    Add code for basic integer sets with array stores

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,23 +1,40 @@
 class MaxIntSet
   def initialize(max)
+    raise ArgumentError if max < 1 || !max.is_a?(Integer)
+    @max = max
+    @store = Array.new(max+1) {false}
   end
 
   def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise "Elements must be integers from 0 to #{@max}"
+    end
   end
 
   def remove(num)
+    if is_valid?(num)
+      @store[num] = false
+    else
+      raise "Elements must be integers from 0 to #{@max}"
+    end
   end
 
   def include?(num)
+    if is_valid?(num)
+      @store[num]
+    else
+      raise "Elements must be integers from 0 to #{@max}"
+    end
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num <= @max && num.is_a?(Integer)
   end
 
-  def validate!(num)
-  end
 end
 
 
@@ -27,18 +44,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
-  private
+  protected
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets

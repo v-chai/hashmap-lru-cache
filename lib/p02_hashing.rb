74376d27d4aebdc7112b84f8ad93f7b33ffa5581
V Prentice <vcoprentice@gmail.com>
2021-11-01 11:12:25 -0700

    Code custom hash methods for Array, String, and Hash classes

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,22 +1,28 @@
 class Integer
-  # Integer#hash already implemented for you
+  # Integer#hash as implemented in Ruby core
 end
 
 class Array
   def hash
-    
+    # Uses bitwise XOR to improve uniformity 
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
+    end
   end
 end
 
 class String
   def hash
+    # Converts to array and maps integer ordinal to each element; 
+    # then uses the Array#hash method defined above
+    chars.map(&:ord).hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
+  # Convert dictionary to array of tuples. Then sort so that ordering is uniform.
+  # Finally, uses Array#hash method defined above to return single hash
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end

ce88c6a317ae728a9a02b85e71ed4645a168e1e6
V Prentice <vcoprentice@gmail.com>
2021-11-01 12:38:26 -0700

    Code linked list implementation

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+include Enumerable
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -16,11 +17,21 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next if @prev
+    @next.prev = @prev if @next
+    @next = nil
+    @prev = nil
+    self
   end
 end
 
 class LinkedList
+  attr_reader :head, :tail
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +40,52 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : self.head.next
   end
 
   def last
+    empty? ? nil : self.tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    any? { |node| node.key == key}
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.prev = self.tail.prev 
+    self.tail.prev.next = new_node
+    new_node.next = self.tail
+    self.tail.prev = new_node
+    new_node
   end
 
   def update(key, val)
+    each {|node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    each {|node| node.remove if node.key == key }
   end
 
   def each
+    ele = @head
+    until ele.next == @tail
+      yield ele.next
+      ele = ele.next
+    end
+
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
